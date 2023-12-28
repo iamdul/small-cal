b538f93c110bbbca08f73ad7d248f30341e21f39
iamdul <kalharidulanga@gmail.com>
2023-12-28 17:29:58 +0530

    Completed basic arithmatic operations

--- main.js
+++ main.js
@@ -1,12 +1,49 @@
 let inputElementObj=document.getElementById("inputEl");
 let numberOne;
 let numberTwo;
+
 inputElementObj.addEventListener('keyup',(event)=>{
     if(event.key==='Enter'|| event.keyCode===13){
-        let tempVal=inputElementObj.ariaValueMax;
+        let tempVal=inputElementObj.value;
         if(!isNaN(tempVal)){
-            numberOne=parseInt(tempVal);
+            if(numberOne==undefined){
+                numberOne=parseInt(tempVal);
+                document.getElementById('num1').textContent=numberOne;
+                inputElementObj.value=0;
+            }else if(numberTwo==undefined){
+                numberTwo=parseInt(tempVal);
+                document.getElementById('num2').textContent=numberTwo;
+                inputElementObj.value=0;
+            }else{
+                alert('Insert again');
+                numberOne=undefined;
+                numberTwo=undefined;
+                document.getElementById('num1').textContent='?';
+                document.getElementById('num2').textContent='?';
+
+            }
+           
         }
+
     }
-})
-//=====functions,operators,switch,if
+});
+
+const execute=(op)=>{
+    if(!numberOne && !numberTwo){
+        alert('Please insert numbers');
+        return
+    }
+    switch(op){
+        case '+':operate((numberOne+numberTwo),op);break;
+        case '-':operate((numberOne-numberTwo),op);break;
+        case '*':operate((numberOne*numberTwo),op);break;
+        case '/':operate((numberOne/numberTwo),op);break;
+        case '%':operate((numberOne%numberTwo),op);
+    }
+};
+
+const operate=(result,op)=>{
+    document.getElementById('op').textContent=op;
+    document.getElementById('ans').textContent=result;  
+};
+
